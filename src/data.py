5d5bd6f862aefe401f1b162aa899a9b665ae7d26
Ansh318 <aragarwal@wisc.edu>
2024-09-14 21:18:18 -0400

    prompts

---
+++ src/data.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+from langchain_openai import OpenAI
+
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+
+
+
+llm = OpenAI()
+
+print(llm.invoke("Hello how are you?"))
---
+++ src/prompts.py
@@ -0,0 +1,15 @@
+from langchain.prompts import PromptTemplate
+
+
+class PromptManager:
+    def __init__(self):
+        """
+        Initalize PromptManager with Prompt Templates for Virtual Librarian.
+        """
+        pass
+
+    def create_prompt(self, template, input_variables):
+        """
+        Initalize and Return Prompt Instance 
+        """
+        pass
