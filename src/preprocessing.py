8bdc06b2e6f3054a45b35e4af0982cd3b7d8e94b
Ansh318 <aragarwal@wisc.edu>
2024-09-19 01:11:13 -0400

    a

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,7 +4,7 @@ df = pd.read_csv("/Users/anshagarwal/GitHub /Virtual Library/Book-Rec-AI/data/Bo
 df = df[['ISBN', 'Book-Title', 'Book-Author', 'Year-Of-Publication', 'Publisher']]
 
 
-empty_str = ""
+empty_list = []
 for index, row in df.iterrows():
     book_title = row['Book-Title']
     book_author = row['Book-Author']
@@ -13,10 +13,11 @@ for index, row in df.iterrows():
     isbn = row['ISBN']
 
     template = f"The Book named {book_title} who's author is {book_author} was published by {publisher} in {year_of_publishement} having ISBN no. {isbn}"
-    empty_str += template + '\n'
+    empty_list.append(template)
 
 
 
 with open("/Users/anshagarwal/GitHub /Virtual Library/Book-Rec-AI/data/books.txt", 'w') as file:
-    file.write(empty_str)
+    for desc in empty_list:
+        file.write(desc + '\n')
     
--- src/vectorDB.py
+++ src/vectorDB.py
@@ -1,18 +1,25 @@
 import weaviate
+from weaviate.classes.config import Configure, Property, DataType
 import os
 
 class WeaviateVectorStore:
 
     def __init__(self):
-        pass
+        self.client = None
 
     def connect_weaviate(self):
-        client = weaviate.connect_to_local(headers = {"X-OpenAI-Api-Key": os.getenv("OPENAI_API_KEY")})
-        client.close()
-        return client
+        self.client = weaviate.connect_to_local(headers = {"X-OpenAI-Api-Key": os.getenv("OPENAI_API_KEY")})
+        self.client.collections.create(
+            "BookInventory",
+            properties = [
+                Property(name = "BookDescription", data_type= DataType.TEXT)
+            ]
+        )
+        self.client.close()
+        return self.client
 
 
-    def read_txt_file(file_path):
+    def read_txt_file(self,file_path):
         """
         Reads a text file and returns its content as a string.
 
@@ -24,8 +31,8 @@ class WeaviateVectorStore:
         """
         try:
             with open(file_path, 'r', encoding='utf-8') as file:
-                content = file.read()
-            return content
+                content = [line.strip() for line in file.readlines()]
+                return content
         except FileNotFoundError:
             return f"Error: The file at {file_path} was not found."
         except Exception as e:
@@ -33,10 +40,28 @@ class WeaviateVectorStore:
 
 
     def write_to_db(self, data):
-        pass
+        for description in data:
+            self.client.data_object.create(
+                {
+                    "Book Description": description
+                },
+                "Book Inventory"
+            )
+
+    def close_connection(self):
+        if self.client != None:
+            self.client.close()
+    
 
 vector_store = WeaviateVectorStore()
-vector_store.connect_weaviate()
+client = vector_store.connect_weaviate()
+# print(client)
+# # inventory = client.collections.get("BookInventory")
+# # print(inventory)
+# data = vector_store.read_txt_file("/Users/anshagarwal/GitHub /Virtual Library/Book-Rec-AI/data/books.txt")
+# # print(data)
+# # vector_store.write_to_db(data)
+# vector_store.close_connection()
 
 
 
